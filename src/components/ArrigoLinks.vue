b9ac5e14cccefd76aadddcb85df88a6000e516fc
Tim Gundmann <tim@gundmann.dk>
2023-09-09 10:01:16 +0200

    added link

--- src/components/ArrigoLinks.vue
+++ src/components/ArrigoLinks.vue
@@ -1,75 +1,107 @@
 <template>
-    <div id="arrigoLinks" class="content-area featured-properties">
+  <div id="arrigoLinks" class="content-area featured-properties">
     <div class="container">
-        <!-- Main title -->
-        <div class="main-title-2">
-            <h1>{{$t('ferielejligheder.firstHeadLine')}}<span>{{$t('ferielejligheder.secondHeadline')}}</span></h1>
-            <p>{{$t('ferielejligheder.subText')}}</p>
-            <div class="title-border">
-                <div class="title-border-inner"></div>
-                <div class="title-border-inner"></div>
-                <div class="title-border-inner"></div>
-                <div class="title-border-inner"></div>
-                <div class="title-border-inner"></div>
-                <div class="title-border-inner"></div>
-                <div class="title-border-inner"></div>
-            </div>
+      <!-- Main title -->
+      <div class="main-title-2">
+        <h1>
+          {{ $t("ferielejligheder.firstHeadLine") }}<span>{{ $t("ferielejligheder.secondHeadline") }}</span>
+        </h1>
+        <p>{{ $t("ferielejligheder.subText") }}</p>
+        <div class="title-border">
+          <div class="title-border-inner"></div>
+          <div class="title-border-inner"></div>
+          <div class="title-border-inner"></div>
+          <div class="title-border-inner"></div>
+          <div class="title-border-inner"></div>
+          <div class="title-border-inner"></div>
+          <div class="title-border-inner"></div>
         </div>
-        <div class="row wow fadeInUp delay-04s">
-            <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
-                <div class="property-3">
-                    <img src="https://bodynsoul.it/wp-content/uploads/2020/02/2A-LOVERSNEST-BEDROOM.jpg" alt="photo" class="img-fluid w-100">
-                    <div class="featured-tag2">Featured</div>
-                    <div class="sale-tag">{{ $t('arrigo.forRent')}}</div>
-                    <div class="ling-section">
-                        <h3>
-                            <a href="https://bodynsoul.it/rooms/room-one/">Lovers’ Nest</a>
-                        </h3>
-                        <ul class="facilities-list">
-                            <li>2 voksne</li>
-                        </ul>
-                        <a href="https://bodynsoul.it/rooms/room-one/" class="read-more-btn">{{ $t('arrigo.readMore')}}</a>
-                    </div>
-                </div>
+      </div>
+      <div class="row wow fadeInUp delay-04s">
+        <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+          <div class="property-3">
+            <img
+              src="https://bodynsoul.it/wp-content/uploads/2020/02/2A-LOVERSNEST-BEDROOM.jpg"
+              alt="photo"
+              class="img-fluid w-100"
+            />
+            <div class="featured-tag2">Featured</div>
+            <div class="sale-tag">{{ $t("arrigo.forRent") }}</div>
+            <div class="ling-section">
+              <h3>
+                <a href="https://bodynsoul.it/rooms/room-one/">Lovers’ Nest</a>
+              </h3>
+              <ul class="facilities-list">
+                <li>2 voksne</li>
+              </ul>
+              <a href="https://bodynsoul.it/rooms/room-one/" class="read-more-btn">{{ $t("arrigo.readMore") }}</a>
             </div>
-            <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
-                <div class="property-3">
-                    <img src="https://bodynsoul.it/wp-content/uploads/2020/02/THESTABLE.jpg" alt="photo" class="img-fluid w-100">
-                    <div class="featured-tag2">Featured</div>
-                    <div class="sale-tag">{{ $t('arrigo.forRent')}}</div>
-                    <div class="ling-section">
-                        <h3>
-                            <a href="https://bodynsoul.it/rooms/room-two/">The Stable</a>
-                        </h3>
-                        <ul class="facilities-list">
-                            <li>2 voksne</li>
-                            <li>2 børn</li>
-                        </ul>
-                        <a href="https://bodynsoul.it/rooms/room-two/" class="read-more-btn">{{ $t('arrigo.readMore')}}</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
-                <div class="property-3">
-                    <img src="https://bodynsoul.it/wp-content/uploads/2020/02/SWALLOWS-NEST-DINING-ROOM.jpg" alt="photo" class="img-fluid w-100">
-                    <div class="featured-tag2">Featured</div>
-                    <div class="sale-tag">{{ $t('arrigo.forRent')}}</div>
-                    <div class="ling-section">
-                        <h3>
-                            <a href="https://bodynsoul.it/rooms/room-three/">Swallows' Nest</a>
-                        </h3>
-                        <ul class="facilities-list">
-                            <li>4 gæster</li>
-                        </ul>
-                        <a href="https://bodynsoul.it/rooms/room-three/" class="read-more-btn">{{ $t('arrigo.readMore')}}</a>
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+          <div class="property-3">
+            <img
+              src="https://bodynsoul.it/wp-content/uploads/2020/02/THESTABLE.jpg"
+              alt="photo"
+              class="img-fluid w-100"
+            />
+            <div class="featured-tag2">Featured</div>
+            <div class="sale-tag">{{ $t("arrigo.forRent") }}</div>
+            <div class="ling-section">
+              <h3>
+                <a href="https://bodynsoul.it/rooms/room-two/">The Stable</a>
+              </h3>
+              <ul class="facilities-list">
+                <li>2 voksne</li>
+                <li>2 børn</li>
+              </ul>
+              <a href="https://bodynsoul.it/rooms/room-two/" class="read-more-btn">{{ $t("arrigo.readMore") }}</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+          <div class="property-3">
+            <img
+              src="https://bodynsoul.it/wp-content/uploads/2020/02/SWALLOWS-NEST-DINING-ROOM.jpg"
+              alt="photo"
+              class="img-fluid w-100"
+            />
+            <div class="featured-tag2">Featured</div>
+            <div class="sale-tag">{{ $t("arrigo.forRent") }}</div>
+            <div class="ling-section">
+              <h3>
+                <a href="https://bodynsoul.it/rooms/room-three/">Swallows' Nest</a>
+              </h3>
+              <ul class="facilities-list">
+                <li>4 gæster</li>
+              </ul>
+              <a href="https://bodynsoul.it/rooms/room-three/" class="read-more-btn">{{ $t("arrigo.readMore") }}</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+          <div class="property-3">
+            <img
+              src="https://bodynsoul.it/wp-content/uploads/2020/02/THE-TOWER-EXTERIOR-2.jpg"
+              alt="photo"
+              class="img-fluid w-100"
+            />
+            <div class="featured-tag2">Featured</div>
+            <div class="sale-tag">{{ $t("arrigo.forRent") }}</div>
+            <div class="ling-section">
+              <h3>
+                <a href="https://bodynsoul.it/rooms/room-four/">The Tower</a>
+              </h3>
+              <ul class="facilities-list">
+                <li>4-6 gæster</li>
+              </ul>
+              <a href="https://bodynsoul.it/rooms/room-three/" class="read-more-btn">{{ $t("arrigo.readMore") }}</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
-
+  </div>
 </template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
